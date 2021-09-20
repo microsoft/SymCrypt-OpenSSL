fc9aa4174ae14754f1b34df84d3e0f94171fc4c1
snofz <spnofzin@microsoft.com>
2021-09-20 14:10:23 -0700

    Moved function descriptions to header and changed one return type to SCOSSL_RETURNLENGTH

--- SymCryptEngine/src/sc_ossl_dh.c
+++ SymCryptEngine/src/sc_ossl_dh.c
@@ -16,11 +16,6 @@ typedef int (*PFN_DH_meth_bn_mod_exp)(const DH* dh, BIGNUM* r,
 typedef int (*PFN_DH_meth_init)(DH* dh);
 typedef int (*PFN_DH_meth_finish)(DH* dh);
 
-// Generates public and private DH values.
-// Expects shared parameters dh->p and dh->g to be set.
-// Generates a random private DH key unless dh->priv_key set, and computes corresponding
-// public value dh->pub_key.
-// Returns 1 on success, 0 otherwise
 SCOSSL_STATUS sc_ossl_dh_generate_key(_Inout_ DH* dh)
 {
     const DH_METHOD* ossl_dh_meth = DH_OpenSSL();
@@ -33,11 +28,7 @@ SCOSSL_STATUS sc_ossl_dh_generate_key(_Inout_ DH* dh)
     return pfn_dh_meth_generate_key(dh);
 }
 
-// Computes the shared secret from the private DH value in dh and the other party's public
-// value in pub_key and stores it in key. key must point to DH_size(dh) bytes of memory.
-// Returns size of shared secret on success, or -1 on error.
-_Success_(return >= 0)
-int sc_ossl_dh_compute_key(_Out_writes_bytes_(DH_size(dh)) unsigned char* key, _In_ const BIGNUM* pub_key, _In_ DH* dh)
+SCOSSL_RETURNLENGTH sc_ossl_dh_compute_key(_Out_writes_bytes_(DH_size(dh)) unsigned char* key, _In_ const BIGNUM* pub_key, _In_ DH* dh)
 {
     const DH_METHOD* ossl_dh_meth = DH_OpenSSL();
 
@@ -49,8 +40,6 @@ int sc_ossl_dh_compute_key(_Out_writes_bytes_(DH_size(dh)) unsigned char* key, _
     return pfn_dh_meth_compute_key(key, pub_key, dh);
 }
 
-// Computes r = a ^ p mod m
-// Returns 1 on success, or 0 on error
 SCOSSL_STATUS sc_ossl_dh_bn_mod_exp(_In_ const DH* dh, _Out_ BIGNUM* r,
     _In_ const BIGNUM* a, _In_ const BIGNUM* p,
     _In_ const BIGNUM* m, _In_ BN_CTX* ctx, _In_ BN_MONT_CTX* m_ctx)
@@ -65,8 +54,6 @@ SCOSSL_STATUS sc_ossl_dh_bn_mod_exp(_In_ const DH* dh, _Out_ BIGNUM* r,
     return pfn_dh_meth_bm_mod_exp(dh, r, a, p, m, ctx, m_ctx);
 }
 
-// Initializes a new DH instance.
-// Returns 1 on success, or 0 on error
 SCOSSL_STATUS sc_ossl_dh_init(_Inout_ DH* dh)
 {
     const DH_METHOD* ossl_dh_meth = DH_OpenSSL();
@@ -79,8 +66,6 @@ SCOSSL_STATUS sc_ossl_dh_init(_Inout_ DH* dh)
     return pfn_dh_meth_init(dh);
 }
 
-// Destroys instance of DH object. The memory for dh is not freed by this function.
-// Returns 1 on success, or 0 on error
 SCOSSL_STATUS sc_ossl_dh_finish(_Inout_ DH* dh)
 {
     const DH_METHOD* ossl_dh_meth = DH_OpenSSL();
--- SymCryptEngine/src/sc_ossl_dsa.c
+++ SymCryptEngine/src/sc_ossl_dsa.c
@@ -14,9 +14,6 @@ typedef int (*PFN_DSA_meth_verify) (const unsigned char* dgst, int dgst_len, DSA
 typedef int (*PFN_DSA_meth_init)(DSA* dsa);
 typedef int (*PFN_DSA_meth_finish)(DSA* dsa);
 
-// Computes a digital signature on the dlen byte message digest dgst using the private key dsa
-// and returns it in a newly allocated DSA_SIG structure.
-// Returns the signature on success, or NULL on error.
 _Success_(return != NULL)
 DSA_SIG* sc_ossl_dsa_sign(_In_reads_bytes_(dlen) const unsigned char* dgst, int dlen, _In_ DSA* dsa)
 {
@@ -29,8 +26,6 @@ DSA_SIG* sc_ossl_dsa_sign(_In_reads_bytes_(dlen) const unsigned char* dgst, int 
     return pfn_dsa_sign(dgst, dlen, dsa);
 }
 
-// Precalculates the DSA signature values k^-1 and r.
-// Returns 1 on success, or 0 on error.
 SCOSSL_STATUS sc_ossl_dsa_sign_setup(_In_ DSA* dsa, _In_ BN_CTX* ctx_in,
     _Out_ BIGNUM** kinvp, _Out_ BIGNUM** rp)
 {
@@ -43,9 +38,6 @@ SCOSSL_STATUS sc_ossl_dsa_sign_setup(_In_ DSA* dsa, _In_ BN_CTX* ctx_in,
     return pfn_dsa_sign_setup(dsa, ctx_in, kinvp, rp);
 }
 
-// Verifies that the signature sig matches a given message digest dgst of size dgst_len.
-// dsa is the signer's public key.
-// Returns 1 for a valid signature, 0 for an incorrect signature, and -1 on error.
 SCOSSL_STATUS sc_ossl_dsa_verify(_In_reads_bytes_(dgst_len) const unsigned char* dgst, int dgst_len,
     _In_ DSA_SIG* sig, _In_ DSA* dsa)
 {
@@ -58,8 +50,6 @@ SCOSSL_STATUS sc_ossl_dsa_verify(_In_reads_bytes_(dgst_len) const unsigned char*
     return pfn_dsa_verify(dgst, dgst_len, sig, dsa);
 }
 
-// Initializes a new DSA instance.
-// Returns 1 on success, or 0 on error
 SCOSSL_STATUS sc_ossl_dsa_init(_Inout_ DSA* dsa)
 {
     const DSA_METHOD* ossl_dsa_meth = DSA_OpenSSL();
@@ -71,9 +61,6 @@ SCOSSL_STATUS sc_ossl_dsa_init(_Inout_ DSA* dsa)
     return pfn_dsa_init(dsa);
 }
 
-
-// Destroys instance of DSA object. The memory for dsa is not freed by this function.
-// Returns 1 on success, or 0 on error
 SCOSSL_STATUS sc_ossl_dsa_finish(_Inout_ DSA* dsa)
 {
     const DSA_METHOD* ossl_dsa_meth = DSA_OpenSSL();
