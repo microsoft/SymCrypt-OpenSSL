415c120a273e51df8d0034ededb37c621b0b76f8
Maxwell Moyer-McKee <mamckee@microsoft.com>
2023-01-10 17:13:29 -0800

    Stub out dispatch tables

--- SymCryptProvider/src/scossl_prov_base.c
+++ SymCryptProvider/src/scossl_prov_base.c
@@ -7,47 +7,77 @@
 #define ALG_TABLE_END { NULL, NULL, NULL, NULL}
 
 static const OSSL_ALGORITHM scossl_prov_digest[] = {
-
+    // ALG("MD5:SSL3-MD5:1.2.840.113549.2.5", scossl_prov_md5_functions),
+    // ALG("SHA1:SHA-1:SSL3-SHA1:1.3.14.3.2.26", scossl_prov_sha1_functions),
+    // ALG("SHA2-256:SHA-256:SHA256:2.16.840.1.101.3.4.2.1", scossl_prov_sha256_functions),
+    // ALG("SHA2-384:SHA-384:SHA384:2.16.840.1.101.3.4.2.2", scossl_prov_sha384_functions),
+    // ALG("SHA2-512:SHA-512:SHA512:2.16.840.1.101.3.4.2.3", scossl_prov_sha512_functions),
+    // ALG("SHA3-256:2.16.840.1.101.3.4.2.8", scossl_prov_sha3_256_functions),
+    // ALG("SHA3-384:2.16.840.1.101.3.4.2.9", scossl_prov_sha3_384_functions),
+    // ALG("SHA3-512:2.16.840.1.101.3.4.2.10", scossl_prov_sha3_512_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_cipher[] = {
-
+    // ALG("AES-128-CBC:AES128:2.16.840.1.101.3.4.1.2", scossl_prov_aes128cbc_functions),
+    // ALG("AES-192-CBC:AES192:2.16.840.1.101.3.4.1.22", scossl_prov_aes192cbc_functions),
+    // ALG("AES-256-CBC:AES256:2.16.840.1.101.3.4.1.42", scossl_prov_aes256cbc_functions),
+    // ALG("AES-128-ECB:2.16.840.1.101.3.4.1.1", scossl_prov_aes128ecb_functions),
+    // ALG("AES-192-ECB:2.16.840.1.101.3.4.1.21", scossl_prov_aes192ecb_functions),
+    // ALG("AES-256-ECB:2.16.840.1.101.3.4.1.41", scossl_prov_aes256ecb_functions),
+    // ALG("AES-128-GCM:id-aes128-GCM:2.16.840.1.101.3.4.1.6", scossl_prov_aes128gcm_functions),
+    // ALG("AES-192-GCM:id-aes192-GCM:2.16.840.1.101.3.4.1.26", scossl_prov_aes192gcm_functions),
+    // ALG("AES-256-GCM:id-aes256-GCM:2.16.840.1.101.3.4.1.46", scossl_prov_aes256gcm_functions),
+    // ALG("AES-128-CCM:id-aes128-CCM:2.16.840.1.101.3.4.1.7", scossl_prov_aes128ccm_functions),
+    // ALG("AES-192-CCM:id-aes192-CCM:2.16.840.1.101.3.4.1.27", scossl_prov_aes192ccm_functions),
+    // ALG("AES-256-CCM:id-aes256-CCM:2.16.840.1.101.3.4.1.47", scossl_prov_aes256ccm_functions),
+    // ALG("AES-128-XTS:1.3.111.2.1619.0.1.1", scossl_prov_aes256xts_functions),
+    // ALG("AES-256-XTS:1.3.111.2.1619.0.1.2", scossl_prov_aes128xts_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_mac[] = {
-
+    // ALG("HMAC", scossl_prov_hmac_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_kdf[] = {
-
+    // ALG("SSHKDF", scossl_prov_sshkdf_kdf_functions),
+    // ALG("HKDF", scossl_prov_hkdf_kdf_functions),
+    // ALG("TLS1-PRF", scossl_prov_tls1prf_kdf_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_rand[] = {
-
+    // ALG("CTR-DRBG", scossl_prov_rand_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_keymgmt[] = {
-
+    // ALG("DH:dhKeyAgreement:1.2.840.113549.1.3.1", scossl_prov_dh_keymgmt_functions),
+    // ALG("RSA:rsaEncryption:1.2.840.113549.1.1.1:", scossl_prov_rsa_keymgmt_functions),
+    // ALG("RSA-PSS:RSASSA-PSS:1.2.840.113549.1.1.10", scossl_prov_rsa_keymgmt_functions),
+    // ALG("EC:id-ecPublicKey:1.2.840.10045.2.1", scossl_prov_ecc_keymgmt_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_keyexch[] = {
-
+    // ALG("DH:dhKeyAgreement:1.2.840.113549.1.3.1", scossl_prov_dh_functions),
+    // ALG("ECDH", scossl_prov_ecdh_functions),
+    // ALG("X25519:1.3.101.110", scossl_prov_x25519_functions),
+    // ALG("HKDF", scossl_prov_keyexch_hkdf_functions),
+    // ALG("TLS1-PRF", scossl_prov_keyexch_tls1prf_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_signature[] = {
-
+    // ALG("RSA-PSS:RSASSA-PSS:1.2.840.113549.1.1.10", scossl_prov_rsa_signature_functions),
+    // ALG("EC:id-ecPublicKey:1.2.840.10045.2.1", scossl_prov_ecdsa_signature_functions),
     ALG_TABLE_END
 };
 
 static const OSSL_ALGORITHM scossl_prov_asym_cipher[] = {
-
+    // ALG("RSA-PSS:RSASSA-PSS:1.2.840.113549.1.1.10", scossl_prov_rsa_asym_cipher_functions),
     ALG_TABLE_END
 };
 
