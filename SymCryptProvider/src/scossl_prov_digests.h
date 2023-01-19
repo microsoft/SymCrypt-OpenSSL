c556a994c89ac0811bdff1f479102ecee4fefd13
Maxwell Moyer-McKee <mamckee@microsoft.com>
2023-01-19 15:51:53 -0800

    Clean up digest implementation based on default provider model

--- SymCryptProvider/src/scossl_prov_digests.h
+++ SymCryptProvider/src/scossl_prov_digests.h
@@ -4,46 +4,57 @@
 #include <openssl/params.h>
 #include "scossl_helpers.h"
 
-#define IMPLEMENT_SCOSSL_DIGEST_FUNCTIONS(alg, CTX, blocksize, size)                                          \
-    static void *scossl_prov_##alg##_newctx(void *prov_ctx)                                                   \
-    {                                                                                                         \
-        return OPENSSL_malloc(sizeof(CTX));                                                                   \
-    }                                                                                                         \
-    static void scossl_prov_##alg##_freectx(CTX *dctx)                                                        \
-    {                                                                                                         \
-        OPENSSL_clear_free(dctx, sizeof(CTX));                                                                \
-    }                                                                                                         \
-    static SCOSSL_STATUS scossl_prov_##alg##_init(CTX *dctx, const OSSL_PARAM params[])                       \
-    {                                                                                                         \
-        SymCrypt##alg##Init(dctx);                                                                            \
-        return SCOSSL_SUCCESS;                                                                                \
-    }                                                                                                         \
-    static SCOSSL_STATUS scossl_prov_##alg##_update(CTX *dctx, const unsigned char *in, size_t inl)           \
-    {                                                                                                         \
-        SymCrypt##alg##Append(dctx, in, inl);                                                                 \
-        return SCOSSL_SUCCESS;                                                                                \
-    }                                                                                                         \
-    static SCOSSL_STATUS scossl_prov_##alg##_final(CTX *dctx, unsigned char *out, size_t *outl, size_t outsz) \
-    {                                                                                                         \
-        if (outsz < size)                                                                                     \
-            return 0;                                                                                         \
-        SymCrypt##alg##Result(dctx, out);                                                                     \
-        *outl = size;                                                                                         \
-        return SCOSSL_SUCCESS;                                                                                \
-    }                                                                                                         \
-    static SCOSSL_STATUS scossl_prov_##alg##_get_params(OSSL_PARAM params[])                                  \
-    {                                                                                                         \
-        return scossl_prov_digest_get_params_common(params, blocksize, size);                                 \
-    }                                                                                                         \
-                                                                                                              \
-    const OSSL_DISPATCH scossl_prov_##alg##_functions[] = {                                                   \
-        {OSSL_FUNC_DIGEST_NEWCTX, (void (*)(void))scossl_prov_##alg##_newctx},                                \
-        {OSSL_FUNC_DIGEST_FREECTX, (void (*)(void))scossl_prov_##alg##_freectx},                              \
-        {OSSL_FUNC_DIGEST_INIT, (void (*)(void))scossl_prov_##alg##_init},                                    \
-        {OSSL_FUNC_DIGEST_UPDATE, (void (*)(void))scossl_prov_##alg##_update},                                \
-        {OSSL_FUNC_DIGEST_FINAL, (void (*)(void))scossl_prov_##alg##_final},                                  \
-        {OSSL_FUNC_DIGEST_GET_PARAMS, (void (*)(void))scossl_prov_##alg##_get_params},                        \
-        {OSSL_FUNC_DIGEST_GETTABLE_PARAMS, (void (*)(void))scossl_prov_digest_gettable_params}};
+// lowercase, CamelCase, and UPPERCASE must be provided to reconcile differences
+// between OpenSSL and SymCrypt APIs and macro definitions
+#define IMPLEMENT_SCOSSL_DIGEST(lcalg, CcAlg, UCALG)                                 \
+    static void *scossl_##lcalg##_newctx(void *prov_ctx)                             \
+    {                                                                                \
+        return OPENSSL_malloc(sizeof(SYMCRYPT_##UCALG##_STATE));                     \
+    }                                                                                \
+    static void scossl_##lcalg##_freectx(SYMCRYPT_##UCALG##_STATE *dctx)             \
+    {                                                                                \
+        OPENSSL_clear_free(dctx, sizeof(SYMCRYPT_##UCALG##_STATE));                  \
+    }                                                                                \
+    static SCOSSL_STATUS scossl_##lcalg##_init(                                      \
+        SYMCRYPT_##UCALG##_STATE *dctx,                                              \
+        const OSSL_PARAM params[])                                                   \
+    {                                                                                \
+        SymCrypt##CcAlg##Init(dctx);                                                 \
+        return SCOSSL_SUCCESS;                                                       \
+    }                                                                                \
+    static SCOSSL_STATUS scossl_##lcalg##_update(                                    \
+        SYMCRYPT_##UCALG##_STATE *dctx,                                              \
+        const unsigned char *in, size_t inl)                                         \
+    {                                                                                \
+        SymCrypt##CcAlg##Append(dctx, in, inl);                                      \
+        return SCOSSL_SUCCESS;                                                       \
+    }                                                                                \
+    static SCOSSL_STATUS scossl_##lcalg##_final(                                     \
+        SYMCRYPT_##UCALG##_STATE *dctx,                                              \
+        unsigned char *out, size_t *outl,                                            \
+        size_t outsz)                                                                \
+    {                                                                                \
+        if (outsz < SYMCRYPT_##UCALG##_RESULT_SIZE)                                  \
+            return 0;                                                                \
+        SymCrypt##CcAlg##Result(dctx, out);                                          \
+        *outl = SYMCRYPT_##UCALG##_RESULT_SIZE;                                      \
+        return SCOSSL_SUCCESS;                                                       \
+    }                                                                                \
+    static SCOSSL_STATUS scossl_##lcalg##_get_params(OSSL_PARAM params[])            \
+    {                                                                                \
+        return scossl_digest_get_params_generic(params,                              \
+                                                SYMCRYPT_##UCALG##_INPUT_BLOCK_SIZE, \
+                                                SYMCRYPT_##UCALG##_RESULT_SIZE);     \
+    }                                                                                \
+                                                                                     \
+    const OSSL_DISPATCH scossl_##lcalg##_functions[] = {                             \
+        {OSSL_FUNC_DIGEST_NEWCTX, (void (*)(void))scossl_##lcalg##_newctx},          \
+        {OSSL_FUNC_DIGEST_FREECTX, (void (*)(void))scossl_##lcalg##_freectx},        \
+        {OSSL_FUNC_DIGEST_INIT, (void (*)(void))scossl_##lcalg##_init},              \
+        {OSSL_FUNC_DIGEST_UPDATE, (void (*)(void))scossl_##lcalg##_update},          \
+        {OSSL_FUNC_DIGEST_FINAL, (void (*)(void))scossl_##lcalg##_final},            \
+        {OSSL_FUNC_DIGEST_GET_PARAMS, (void (*)(void))scossl_##lcalg##_get_params},  \
+        {OSSL_FUNC_DIGEST_GETTABLE_PARAMS, (void (*)(void))scossl_digest_gettable_params_generic}};
 
-const OSSL_PARAM *scossl_prov_digest_gettable_params(void *dctx, void *provctx);
-SCOSSL_STATUS scossl_prov_digest_get_params_common(OSSL_PARAM params[], size_t blocksize, size_t size);
+const OSSL_PARAM *scossl_digest_gettable_params_generic(void *dctx, void *provctx);
+SCOSSL_STATUS scossl_digest_get_params_generic(OSSL_PARAM params[], size_t blocksize, size_t size);
