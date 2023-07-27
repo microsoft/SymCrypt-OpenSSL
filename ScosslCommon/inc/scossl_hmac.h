6773d38a66d075a973d7b151a624a3cc748bdfe8
Maxwell Moyer-McKee <mamckee@microsoft.com>
2023-07-27 15:07:00 -0700

    Common hmac implementation

--- ScosslCommon/inc/scossl_hmac.h
+++ ScosslCommon/inc/scossl_hmac.h
@@ -12,20 +12,25 @@ typedef struct {
     SYMCRYPT_MAC_EXPANDED_KEY   expandedKey;
     SYMCRYPT_MAC_STATE          macState;
     PCSYMCRYPT_MAC              pMac;
-    ASN1_OCTET_STRING           pbKey;
+    ASN1_OCTET_STRING           key;
 } SCOSSL_HMAC_CTX;
 
-SCOSSL_HMAC_CTX *scossl_hmac_newctx();
-SCOSSL_HMAC_CTX *scossl_hmac_dupctx(_In_ SCOSSL_HMAC_CTX *ctx);
-void scossl_hmac_freectx(_Inout_ SCOSSL_HMAC_CTX *ctx);
+// The HMAC context contains SymCrypt structures that must be properly
+// aligned. Because of this, these functions use the pointer to the aligned
+// memory rather than the SCOSSL_HMAC_CTX directly. Callers can use
+// SCOSSL_ALIGN_UP if they need to access the SCOSSL_HMAC_CTX.
+PBYTE scossl_hmac_newctx();
+PBYTE scossl_hmac_dupctx(_In_ PBYTE alignedCtx);
+void scossl_hmac_freectx(_Inout_ PBYTE alignedCtx);
 
-SCOSSL_STATUS scossl_hmac_set_mac_key(_Inout_ SCOSSL_HMAC_CTX *ctx,
-                                      _In_reads_bytes_(cbMacKey) PBYTE pbMacKey, SIZE_T cbMacKey);
-SCOSSL_STATUS scossl_hmac_init(_Inout_ SCOSSL_HMAC_CTX *ctx,
-                               _In_reads_bytes_(cbKey) PBYTE pbKey, SIZE_T cbKey);
-SCOSSL_STATUS scossl_hmac_update(_Inout_ SCOSSL_HMAC_CTX *ctx,
+SCOSSL_STATUS scossl_hmac_set_md(_Inout_ PBYTE alignedCtx, _In_ const EVP_MD *md);
+SCOSSL_STATUS scossl_hmac_set_mac_key(_Inout_ PBYTE alignedCtx,
+                                      _In_reads_bytes_(cbMacKey) const char *macKey, SIZE_T cbMacKey);
+SCOSSL_STATUS scossl_hmac_init(_Inout_ PBYTE alignedCtx,
+                               _In_reads_bytes_(cbKey) PCBYTE pbKey, SIZE_T cbKey);
+SCOSSL_STATUS scossl_hmac_update(_Inout_ PBYTE alignedCtx,
                                  _In_reads_bytes_(cbData) PCBYTE pbData, SIZE_T cbData);
-SCOSSL_STATUS scossl_hmac_final(_Inout_ SCOSSL_HMAC_CTX *ctx,
+SCOSSL_STATUS scossl_hmac_final(_Inout_ PBYTE alignedCtx,
                                 _Out_writes_bytes_(*cbResult) PBYTE pbResult, _Out_ SIZE_T *cbResult);
 
 #ifdef __cplusplus
--- ScosslCommon/src/scossl_hmac.c
+++ ScosslCommon/src/scossl_hmac.c
@@ -8,51 +8,181 @@
 extern "C" {
 #endif
 
-SCOSSL_HMAC_CTX *scossl_hmac_newctx()
+PBYTE scossl_hmac_newctx()
 {
-    return NULL;
+    return OPENSSL_zalloc(SCOSSL_ALIGNED_SIZEOF(SCOSSL_HMAC_CTX));
+}
+
+#define SCOSSL_COPY_HMAC_KEY_AND_STATE(lcAlg, UcAlg)                   \
+    SymCryptHmac##UcAlg##KeyCopy(&ctx->expandedKey.##lcAlg##Key,       \
+                                 &copyCtx->expandedKey.##lcAlg##Key);  \
+    SymCryptHmac##UcAlg##StateCopy(&ctx->macState.##lcAlg##State,      \
+                                   &copyCtx->expandedKey.##lcAlg##Key, \
+                                   &copyCtx->macState.##lcAlg##State);
+
+_Use_decl_annotations_
+PBYTE scossl_hmac_dupctx(PBYTE alignedCtx)
+{
+    SCOSSL_HMAC_CTX *ctx, *copyCtx;
+    ASN1_OCTET_STRING *pkey;
+    PBYTE alignedCopy;
+
+    if ((alignedCopy = scossl_hmac_newctx()) == NULL)
+    {
+        return NULL;
+    }
+
+    ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
+    copyCtx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCopy);
+
+    if (ctx->key.data != NULL)
+    {
+        if ((pkey = ASN1_OCTET_STRING_dup(&ctx->key)) == NULL)
+        {
+            scossl_hmac_freectx(copyCtx);
+            return NULL;
+        }
+
+        copyCtx->key = *pkey;
+    }
+
+    copyCtx->pMac = ctx->pMac;
+
+
+    // Copy the expanded key and mac state
+    if(ctx->pMac == SymCryptHmacSha1Algorithm)
+    {
+        SCOSSL_COPY_HMAC_KEY_AND_STATE(sha1, Sha1);
+    }
+    else if(ctx->pMac == SymCryptHmacSha256Algorithm)
+    {
+        SCOSSL_COPY_HMAC_KEY_AND_STATE(sha256, Sha256);
+    }
+    else if(ctx->pMac == SymCryptHmacSha384Algorithm)
+    {
+        SCOSSL_COPY_HMAC_KEY_AND_STATE(sha384, Sha384);
+    }
+    else if(ctx->pMac == SymCryptHmacSha512Algorithm)
+    {
+        SCOSSL_COPY_HMAC_KEY_AND_STATE(sha512, Sha512);
+    }
+
+    return alignedCopy;
 }
 
 _Use_decl_annotations_
-SCOSSL_HMAC_CTX *scossl_hmac_dupctx(SCOSSL_HMAC_CTX *ctx)
+void scossl_hmac_freectx(PBYTE alignedCtx)
 {
-    return NULL;
+    if (alignedCtx == NULL)
+        return;
+
+    SCOSSL_HMAC_CTX *ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
+
+    OPENSSL_clear_free(ctx->key.data, ctx->key.length);
+    OPENSSL_clear_free(alignedCtx, SCOSSL_ALIGNED_SIZEOF(SCOSSL_HMAC_CTX));
 }
 
 _Use_decl_annotations_
-void scossl_hmac_freectx(SCOSSL_HMAC_CTX *ctx)
+SCOSSL_STATUS scossl_hmac_set_md(PBYTE alignedCtx, const EVP_MD *md)
 {
+    SCOSSL_HMAC_CTX *ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
+    SCOSSL_STATUS ret = SCOSSL_SUCCESS;
+    int type = EVP_MD_type(md);
 
+    switch (type)
+    {
+    case NID_sha1:
+        ctx->pMac = SymCryptHmacSha1Algorithm;
+        break;
+    case NID_sha256:
+        ctx->pMac = SymCryptHmacSha256Algorithm;
+        break;
+    case NID_sha384:
+        ctx->pMac = SymCryptHmacSha384Algorithm;
+        break;
+    case NID_sha512:
+        ctx->pMac = SymCryptHmacSha512Algorithm;
+        break;
+    default:
+        SCOSSL_LOG_ERROR(SCOSSL_ERR_F_GET_SYMCRYPT_HASH_ALGORITHM, SCOSSL_ERR_R_NOT_IMPLEMENTED,
+                         "SymCrypt engine does not support hash algorithm %d", type);
+        ret = SCOSSL_FAILURE;
+    }
+
+    return ret;
 }
 
 _Use_decl_annotations_
-SCOSSL_STATUS scossl_hmac_set_mac_key(SCOSSL_HMAC_CTX *ctx,
-                                      PBYTE pbMacKey, SIZE_T cbMacKey)
+SCOSSL_STATUS scossl_hmac_set_mac_key(PBYTE alignedCtx,
+                                      const char *macKey, SIZE_T cbMacKey)
 {
+    SCOSSL_HMAC_CTX *ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
+
+    if (cbMacKey < -1 || macKey == NULL)
+    {
+        return SCOSSL_FAILURE;
+    }
+
+    if (ctx->key.data != NULL)
+    {
+        OPENSSL_clear_free(ctx->key.data, ctx->key.length);
+    }
+
+    return ASN1_OCTET_STRING_set(&ctx->key, macKey, cbMacKey);
+}
+
+_Use_decl_annotations_
+SCOSSL_STATUS scossl_hmac_init(PBYTE alignedCtx,
+                               PCBYTE pbKey, SIZE_T cbKey)
+{
+    SCOSSL_HMAC_CTX *ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
+
+    if(ctx->pMac->expandKeyFunc(&ctx->expandedKey, pbKey, cbKey) != SYMCRYPT_NO_ERROR)
+    {
+        return SCOSSL_FAILURE;
+    }
+
+    ctx->pMac->initFunc(&ctx->macState, &ctx->expandedKey);
+
     return SCOSSL_FAILURE;
 }
 
 _Use_decl_annotations_
-SCOSSL_STATUS scossl_hmac_init(SCOSSL_HMAC_CTX *ctx,
-                               PBYTE pbKey, SIZE_T cbKey)
+SCOSSL_STATUS scossl_hmac_update(PBYTE alignedCtx,
+                                 PCBYTE pbData, SIZE_T cbData)
 {
-    return SCOSSL_FAILURE;
+    SCOSSL_HMAC_CTX *ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
+
+    ctx->pMac->appendFunc(&ctx->macState, pbData, cbData);
+
+    return SCOSSL_SUCCESS;
 }
 
 _Use_decl_annotations_
-SCOSSL_STATUS scossl_hmac_update(SCOSSL_HMAC_CTX *ctx,
-                                PCBYTE pbData, SIZE_T cbData)
-{
-    return SCOSSL_FAILURE;
-}
-
-_Use_decl_annotations_
-SCOSSL_STATUS scossl_hmac_final(SCOSSL_HMAC_CTX *ctx,
+SCOSSL_STATUS scossl_hmac_final(PBYTE alignedCtx,
                                 PBYTE pbResult, SIZE_T *cbResult)
 {
-    return SCOSSL_FAILURE;
-}
+    SCOSSL_STATUS ret = SCOSSL_SUCCESS;
+    SCOSSL_HMAC_CTX *ctx = (SCOSSL_HMAC_CTX *)SCOSSL_ALIGN_UP(alignedCtx);
 
+    if (!pbResult)
+    {
+        *cbResult = ctx->pMac->resultSize;
+        return SCOSSL_SUCCESS;
+    }
+
+    if (*cbResult < ctx->pMac->resultSize)
+    {
+
+        return SCOSSL_FAILURE;
+
+    }
+
+    ctx->pMac->resultFunc(&ctx->macState, pbResult);
+    *cbResult = ctx->pMac->resultSize;
+
+    return SCOSSL_SUCCESS;
+}
 
 #ifdef __cplusplus
 }
