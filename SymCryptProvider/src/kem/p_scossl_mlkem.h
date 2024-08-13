cf76ab5c0025e1256cadc0d0460447b80f5e3b80
Maxwell Moyer-McKee <mamckee@microsoft.com>
2024-08-13 23:38:05 +0000

    Boilerplate mlkem kem and keymgmt interfaces

---
+++ SymCryptProvider/src/kem/p_scossl_mlkem.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Microsoft Corporation. Licensed under the MIT license.
+//
+
+#include "scossl_helpers.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    BOOL initialized;
+} SCOSSL_MLKEM_KEY_CTX;
+
+#ifdef __cplusplus
+}
+#endif
