9ddf8c7810eb72269a86ea11f050eeb377079eb9
Maxwell Moyer-McKee <mamckee@microsoft.com>
2024-09-06 23:29:24 +0000

    Context and parameters

--- SymCryptProvider/src/kdf/p_scossl_srtpkdf.c
+++ SymCryptProvider/src/kdf/p_scossl_srtpkdf.c
@@ -2,40 +2,54 @@
 // Copyright (c) Microsoft Corporation. Licensed under the MIT license.
 //
 
-#include "scossl_srtpkdf.h"
-#include "p_scossl_base.h"
-
+#include <openssl/core_names.h>
 #include <openssl/proverr.h>
 
+#include "scossl_helpers.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define SCOSSL_SRTP_KDF_SALT_SIZE (112 / 8)
+
+#define SCOSSL_KDF_PARAM_SRTP_RATE "rate"
+#define SCOSSL_KDF_PARAM_SRTP_INDEX "index"
+#define SCOSSL_KDF_PARAM_SRTP_INDEX_WIDTH "index-width"
+
 typedef struct
 {
+    PBYTE pKey;
+    SIZE_T cbKey;
+    SYMCRYPT_SRTPKDF_EXPANDED_KEY expandedKey;
 
+    BYTE pbSalt[SCOSSL_SRTP_KDF_SALT_SIZE];
+    BOOL isSaltSet;
+
+    UINT32 uKeyDerivationRate;
+    UINT64 uIndex;
+    UINT32 uIndexWidth;
+    BYTE label;
 } SCOSSL_PROV_SRTPKDF_CTX;
 
-
 static const OSSL_PARAM p_scossl_srtpkdf_gettable_ctx_param_types[] = {
+    OSSL_PARAM_size_t(OSSL_KDF_PARAM_SIZE, NULL),
     OSSL_PARAM_END};
 
 static const OSSL_PARAM p_scossl_srtpkdf_settable_ctx_param_types[] = {
+    OSSL_PARAM_octet_string(OSSL_KDF_PARAM_KEY, NULL, 0),
+    OSSL_PARAM_octet_string(OSSL_KDF_PARAM_SALT, NULL, 0),
+    OSSL_PARAM_uint(SCOSSL_KDF_PARAM_SRTP_RATE, NULL),
+    OSSL_PARAM_uint(SCOSSL_KDF_PARAM_SRTP_INDEX, NULL),
+    OSSL_PARAM_uint(SCOSSL_KDF_PARAM_SRTP_INDEX_WIDTH, NULL),
+    OSSL_PARAM_utf8_string(OSSL_KDF_PARAM_LABEL, NULL, 0),
     OSSL_PARAM_END};
 
 static SCOSSL_STATUS p_scossl_srtpkdf_set_ctx_params(_Inout_ SCOSSL_PROV_SRTPKDF_CTX *ctx, const _In_ OSSL_PARAM params[]);
 
-static SCOSSL_PROV_SRTPKDF_CTX *p_scossl_srtpkdf_newctx(ossl_unused SCOSSL_PROVCTX *provctx)
+static SCOSSL_PROV_SRTPKDF_CTX *p_scossl_srtpkdf_newctx(ossl_unused void *provctx)
 {
-    SCOSSL_PROV_SRTPKDF_CTX *ctx = OPENSSL_malloc(sizeof(SCOSSL_PROV_SRTPKDF_CTX));
-
-    if (ctx != NULL)
-    {
-
-    }
-
-    return ctx;
+    return OPENSSL_zalloc(sizeof(SCOSSL_PROV_SRTPKDF_CTX));
 }
 
 static void p_scossl_srtpkdf_freectx(_Inout_ SCOSSL_PROV_SRTPKDF_CTX *ctx)
@@ -43,16 +57,54 @@ static void p_scossl_srtpkdf_freectx(_Inout_ SCOSSL_PROV_SRTPKDF_CTX *ctx)
     if (ctx == NULL)
         return;
 
+    SymCryptWipeKnownSize(&ctx->expandedKey, sizeof(SYMCRYPT_SRTPKDF_EXPANDED_KEY));
+    OPENSSL_secure_clear_free(ctx->pKey, ctx->cbKey);
     OPENSSL_free(ctx);
 }
 
 static SCOSSL_PROV_SRTPKDF_CTX *p_scossl_srtpkdf_dupctx(_In_ SCOSSL_PROV_SRTPKDF_CTX *ctx)
 {
+    SCOSSL_STATUS status = SCOSSL_FAILURE;
     SCOSSL_PROV_SRTPKDF_CTX *copyCtx = OPENSSL_malloc(sizeof(SCOSSL_PROV_SRTPKDF_CTX));
 
     if (copyCtx != NULL)
     {
+        if (ctx->pKey != NULL)
+        {
+            if ((copyCtx->pKey = OPENSSL_secure_malloc(ctx->cbKey)) == NULL)
+            {
+                ERR_raise(ERR_LIB_PROV, ERR_R_MALLOC_FAILURE);
+                goto cleanup;
+            }
 
+            memcpy(copyCtx->pKey, ctx->pKey, ctx->cbKey);
+            copyCtx->cbKey = ctx->cbKey;
+        }
+        else
+        {
+            copyCtx->pKey = NULL;
+            copyCtx->cbKey = 0;
+        }
+
+        if (ctx->isSaltSet)
+        {
+            memcpy(copyCtx->pbSalt, ctx->pbSalt, SCOSSL_SRTP_KDF_SALT_SIZE);
+        }
+
+        copyCtx->isSaltSet = ctx->isSaltSet;
+        copyCtx->uKeyDerivationRate = ctx->uKeyDerivationRate;
+        copyCtx->uIndex = ctx->uIndex;
+        copyCtx->uIndexWidth = ctx->uIndexWidth;
+        copyCtx->label = ctx->label;
+    }
+
+    status = SCOSSL_SUCCESS;
+
+cleanup:
+    if (status == SCOSSL_FAILURE)
+    {
+        p_scossl_srtpkdf_freectx(copyCtx);
+        copyCtx = NULL;
     }
 
     return copyCtx;
@@ -64,8 +116,8 @@ static SCOSSL_STATUS p_scossl_srtpkdf_reset(_Inout_ SCOSSL_PROV_SRTPKDF_CTX *ctx
 }
 
 static SCOSSL_STATUS p_scossl_srtpkdf_derive(_In_ SCOSSL_PROV_SRTPKDF_CTX *ctx,
-                                          _Out_writes_bytes_(keylen) unsigned char *key, size_t keylen,
-                                          _In_ const OSSL_PARAM params[])
+                                             _Out_writes_bytes_(keylen) unsigned char *key, size_t keylen,
+                                             _In_ const OSSL_PARAM params[])
 {
     return SCOSSL_FAILURE;
 }
