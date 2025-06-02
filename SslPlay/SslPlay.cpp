4b50f512fc60a4cb6d1cad415aebe4fef7ec40d6
liumegan <liumegan@microsoft.com>
2025-06-02 19:38:52 +0000

    SCOSSL-brainpool: add t1 support

--- SslPlay/SslPlay.cpp
+++ SslPlay/SslPlay.cpp
@@ -164,14 +164,11 @@ void TestEcc()
     TestEccCurve(NID_secp384r1);
     TestEccCurve(NID_secp521r1);
     TestEccCurve(NID_brainpoolP256r1);
-    /*
-    TestEccCurve(NID_brainpoolP256r1);
     TestEccCurve(NID_brainpoolP384r1);
     TestEccCurve(NID_brainpoolP512r1);
-    TestEccCurve(NID_brainpoolP256r1tls13);
-    TestEccCurve(NID_brainpoolP384r1tls13);
-    TestEccCurve(NID_brainpoolP512r1tls13);
-    */
+    TestEccCurve(NID_brainpoolP256t1);
+    TestEccCurve(NID_brainpoolP384t1);
+    TestEccCurve(NID_brainpoolP512t1);
     printf("%s", SeparatorLine);
 }
 
