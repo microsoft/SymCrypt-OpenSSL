b2970d22dbc0bf0a42e5e033865b5898a41eb280
Maxwell Moyer-McKee <mamckee@microsoft.com>
2025-06-03 19:21:52 +0000

    PR comments and allow caller to fetch Key ID

--- KeysInUse/keysinuse.h
+++ KeysInUse/keysinuse.h
@@ -12,7 +12,7 @@ typedef enum
 {
     KEYSINUSE_SIGN,
     KEYSINUSE_DECRYPT
-} keysinuse_operation;
+} KEYSINUSE_OPERATION;
 
 typedef void SCOSSL_KEYSINUSE_CTX;  // Exported opaque type
 
@@ -45,17 +45,31 @@ int keysinuse_is_running();
 void keysinuse_set_max_file_size(long size);
 void keysinuse_set_logging_delay(long delay);
 
+// Computes the key identifier for the encoded public key pbEncodedKey.
+// Writes the result of to pbKeyIdentifier, which must be large enough
+// to hold the key identifier. If pbKeyIdentifier is NULL, the function will
+// return the minimum size required to hold the key identifier.
+// On success, the function returns the number of bytes written to pbKeyIdentifier.
+unsigned int keysinuse_derive_key_identifier(const void *pbEncodedKey, unsigned long cbEncodedKey,
+                                             char *pbKeyIdentifier, unsigned long cbKeyIdentifier);
+
 //
 // KeysInUse context management
 //
 SCOSSL_KEYSINUSE_CTX *keysinuse_load_key(const void *pbEncodedKey, unsigned long cbEncodedKey);
-SCOSSL_KEYSINUSE_CTX *keysinuse_load_key_by_ctx(SCOSSL_KEYSINUSE_CTX *keysinuseCtx);
-void keysinuse_unload_key(SCOSSL_KEYSINUSE_CTX *keysinuseCtx);
+SCOSSL_KEYSINUSE_CTX *keysinuse_load_key_by_ctx(SCOSSL_KEYSINUSE_CTX *ctx);
+void keysinuse_unload_key(SCOSSL_KEYSINUSE_CTX *ctx);
+
+// Copies the key identifier from an existing keysinuseCtx to pbKeyIdentifier,
+// If pbKeyIdentifier is NULL, the function will return the minimum size required
+// to hold the key identifier.
+// On success, the function returns the number of bytes written to pbKeyIdentifier.
+unsigned int keysinuse_ctx_get_key_identifier(SCOSSL_KEYSINUSE_CTX *ctx, char *pbKeyIdentifier, unsigned long cbKeyIdentifier);
 
 //
 // Usage tracking
 //
-void keysinuse_on_use(SCOSSL_KEYSINUSE_CTX *keysinuseInfo, keysinuse_operation operation);
+void keysinuse_on_use(SCOSSL_KEYSINUSE_CTX *ctx, KEYSINUSE_OPERATION operation);
 
 #ifdef __cplusplus
 }
