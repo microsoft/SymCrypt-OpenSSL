afa2499fcd5b983748b7a30dfa190e710c719e7f
Maxwell Moyer-McKee <mamckee@microsoft.com>
2026-04-20 19:03:20 +0000

    Add RSA sigalg interface

--- SymCryptProvider/src/signature/p_scossl_rsa_signature.c
+++ SymCryptProvider/src/signature/p_scossl_rsa_signature.c
@@ -35,6 +35,16 @@ typedef struct
     const OSSL_ITEM *mgf1MdInfo; // Informational, must match md if set
     int cbSalt;
     int cbSaltMin;
+
+    // Sigalg state tracking
+    BOOL isSigalg;
+    BOOL allowUpdate;
+    BOOL allowFinal;
+    BOOL allowOneshot;
+
+    // Sigalg verify message support
+    PBYTE pbSignature;
+    SIZE_T cbSignature;
 } SCOSSL_RSA_SIGN_CTX;
 
 #define SCOSSL_RSA_SIGNATURE_GETTABLE_PARAMS                        \
@@ -58,6 +68,10 @@ static const OSSL_PARAM p_scossl_rsa_sig_ctx_gettable_param_types[] = {
 static const OSSL_PARAM p_scossl_rsa_pss_sig_ctx_gettable_param_types[] = {
     SCOSSL_RSA_SIGNATURE_GETTABLE_PARAMS
     SCOSSL_RSA_PSS_SIGNATURE_GETTABLE_PARAMS
+    OSSL_PARAM_END};
+
+static const OSSL_PARAM p_scossl_rsa_sigalg_ctx_settable_param_types[] = {
+    OSSL_PARAM_octet_string(OSSL_SIGNATURE_PARAM_SIGNATURE, NULL, 0),
     OSSL_PARAM_END};
 
 // Padding may not be set at the time of querying settable params, so PSS params
@@ -112,6 +126,7 @@ static void p_scossl_rsa_freectx(SCOSSL_RSA_SIGN_CTX *ctx)
     EVP_MD_CTX_free(ctx->mdctx);
     EVP_MD_free(ctx->md);
     OPENSSL_free(ctx->propq);
+    OPENSSL_free(ctx->pbSignature);
     OPENSSL_free(ctx);
 }
 
@@ -152,12 +167,12 @@ static SCOSSL_STATUS p_scossl_rsa_signverify_init(_Inout_ SCOSSL_RSA_SIGN_CTX *c
         return SCOSSL_FAILURE;
     }
 
-#ifdef RSA_PSS_SALTLEN_AUTO_DIGEST_MAX
     ctx->cbSalt = RSA_PSS_SALTLEN_AUTO_DIGEST_MAX;
-#else
-    ctx->cbSalt = RSA_PSS_SALTLEN_AUTO;
-#endif
     ctx->operation = operation;
+    ctx->allowUpdate = TRUE;
+    ctx->allowFinal = TRUE;
+    ctx->allowOneshot = TRUE;
+
     if (keyCtx != NULL)
     {
         if (!keyCtx->initialized)
@@ -402,7 +417,7 @@ static SCOSSL_STATUS p_scossl_rsa_digest_sign_final(_In_ SCOSSL_RSA_SIGN_CTX *ct
     }
 
     // If sig is NULL, this is a size fetch, and the digest does not need to be computed
-    if (sig == NULL || EVP_DigestFinal(ctx->mdctx, digest, &cbDigest))
+    if (sig == NULL || EVP_DigestFinal_ex(ctx->mdctx, digest, &cbDigest))
     {
         ctx->allowMdUpdates = sig != NULL;
         ret = p_scossl_rsa_sign(ctx, sig, siglen, sigsize, digest, cbDigest);
@@ -424,14 +439,19 @@ static SCOSSL_STATUS p_scossl_rsa_digest_verify_final(_In_ SCOSSL_RSA_SIGN_CTX *
 
     ctx->allowMdUpdates = TRUE;
 
-    return EVP_DigestFinal(ctx->mdctx, digest, &cbDigest) &&
+    return EVP_DigestFinal_ex(ctx->mdctx, digest, &cbDigest) &&
            p_scossl_rsa_verify(ctx, sig, siglen, digest, cbDigest);
 }
 
 static const OSSL_PARAM *p_scossl_rsa_settable_ctx_params(_In_ SCOSSL_RSA_SIGN_CTX *ctx,
                                                           ossl_unused void *provctx)
 {
-    return ctx->allowMdUpdates ? p_scossl_rsa_sig_ctx_settable_param_types : p_scossl_rsa_sig_ctx_settable_param_types_no_digest;
+    if (ctx != NULL && !ctx->allowMdUpdates)
+    {
+        return p_scossl_rsa_sig_ctx_settable_param_types_no_digest;
+    }
+
+    return p_scossl_rsa_sig_ctx_settable_param_types;
 }
 
 static SCOSSL_STATUS p_scossl_rsa_set_ctx_params(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx, _In_ const OSSL_PARAM params[])
@@ -581,14 +601,12 @@ static SCOSSL_STATUS p_scossl_rsa_set_ctx_params(_Inout_ SCOSSL_RSA_SIGN_CTX *ct
             {
                 cbSalt = RSA_PSS_SALTLEN_MAX;
             }
-#ifdef RSA_PSS_SALTLEN_AUTO_DIGEST_MAX
             else if (strcmp(p->data, OSSL_PKEY_RSA_PSS_SALT_LEN_AUTO_DIGEST_MAX) == 0)
             {
                 // Sign: Smaller of digest length or maximized salt length
                 // Verify: Autorecovered salt length
                 cbSalt = RSA_PSS_SALTLEN_AUTO_DIGEST_MAX;
             }
-#endif
             else
             {
                 cbSalt = atoi(p->data);
@@ -598,11 +616,7 @@ static SCOSSL_STATUS p_scossl_rsa_set_ctx_params(_Inout_ SCOSSL_RSA_SIGN_CTX *ct
             ERR_raise(ERR_LIB_PROV, PROV_R_FAILED_TO_GET_PARAMETER);
             return SCOSSL_FAILURE;
         }
-#ifdef RSA_PSS_SALTLEN_AUTO_DIGEST_MAX
         if (cbSalt < RSA_PSS_SALTLEN_AUTO_DIGEST_MAX)
-#else
-        if (cbSalt < RSA_PSS_SALTLEN_MAX)
-#endif
         {
             ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_SALT_LENGTH);
             return SCOSSL_FAILURE;
@@ -613,9 +627,7 @@ static SCOSSL_STATUS p_scossl_rsa_set_ctx_params(_Inout_ SCOSSL_RSA_SIGN_CTX *ct
             switch (cbSalt)
             {
             case RSA_PSS_SALTLEN_AUTO:
-#ifdef RSA_PSS_SALTLEN_AUTO_DIGEST_MAX
             case RSA_PSS_SALTLEN_AUTO_DIGEST_MAX:
-#endif
                 if (ctx->operation == EVP_PKEY_OP_VERIFY)
                 {
                     ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_SALT_LENGTH);
@@ -699,6 +711,40 @@ static SCOSSL_STATUS p_scossl_rsa_set_ctx_params(_Inout_ SCOSSL_RSA_SIGN_CTX *ct
     return SCOSSL_SUCCESS;
 }
 
+static const OSSL_PARAM *p_scossl_rsa_sigalg_settable_ctx_params(_In_ SCOSSL_RSA_SIGN_CTX *ctx, ossl_unused void *provctx)
+{
+    if (ctx != NULL && ctx->operation != EVP_PKEY_OP_VERIFYMSG)
+    {
+        return p_scossl_rsa_sigalg_ctx_settable_param_types;
+    }
+
+    return NULL;
+}
+
+static SCOSSL_STATUS p_scossl_rsa_sigalg_set_ctx_params(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx, _In_ const OSSL_PARAM params[])
+{
+    const OSSL_PARAM *p;
+
+    if (ctx == NULL)
+        return SCOSSL_FAILURE;
+
+    if (ctx->operation == EVP_PKEY_OP_VERIFYMSG)
+    {
+        if ((p = OSSL_PARAM_locate_const(params, OSSL_SIGNATURE_PARAM_SIGNATURE)) != NULL)
+        {
+            OPENSSL_free(ctx->pbSignature);
+            ctx->pbSignature = NULL;
+            ctx->cbSignature = 0;
+            if (!OSSL_PARAM_get_octet_string(p, (void **)&ctx->pbSignature, 0, &ctx->cbSignature))
+            {
+                return SCOSSL_FAILURE;
+            }
+        }
+    }
+
+    return SCOSSL_SUCCESS;
+}
+
 static const OSSL_PARAM *p_scossl_rsa_gettable_ctx_params(_In_ SCOSSL_RSA_SIGN_CTX  *ctx,
                                                           ossl_unused void *provctx)
 {
@@ -768,11 +814,9 @@ static ASN1_STRING *p_scossl_rsa_pss_params_to_asn1_sequence(_In_ SCOSSL_RSA_SIG
         case RSA_PSS_SALTLEN_AUTO:
             cbSalt = cbSaltMax;
             break;
-#ifdef RSA_PSS_SALTLEN_AUTO_DIGEST_MAX
         case RSA_PSS_SALTLEN_AUTO_DIGEST_MAX:
             cbSalt = cbSaltMax < (int)cbHash ? cbSaltMax : (int)cbHash;
             break;
-#endif
         }
 
         if (cbSalt < 0)
@@ -981,11 +1025,9 @@ static SCOSSL_STATUS p_scossl_rsa_get_ctx_params(_In_ SCOSSL_RSA_SIGN_CTX *ctx, 
                 case RSA_PSS_SALTLEN_MAX:
                     saltLenText = OSSL_PKEY_RSA_PSS_SALT_LEN_MAX;
                     break;
-#ifdef RSA_PSS_SALTLEN_AUTO_DIGEST_MAX
                 case RSA_PSS_SALTLEN_AUTO_DIGEST_MAX:
                     saltLenText = OSSL_PKEY_RSA_PSS_SALT_LEN_AUTO_DIGEST_MAX;
                     break;
-#endif
                 default:
                     len = BIO_snprintf(p->data, p->data_size, "%d",
                                            ctx->cbSalt);
@@ -1087,6 +1129,222 @@ const OSSL_DISPATCH p_scossl_rsa_signature_functions[] = {
     {OSSL_FUNC_SIGNATURE_SET_CTX_MD_PARAMS, (void (*)(void))p_scossl_rsa_set_ctx_md_params},
     {OSSL_FUNC_SIGNATURE_SETTABLE_CTX_MD_PARAMS, (void (*)(void))p_scossl_rsa_settable_ctx_md_params},
     {0, NULL}};
+
+static SCOSSL_STATUS p_scossl_rsa_sigalg_signverify_init(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx, _In_ SCOSSL_PROV_RSA_KEY_CTX *keyCtx,
+                                                         _In_ const OSSL_PARAM params[], int operation,
+                                                         _In_ const char *mdname)
+{
+    EVP_MD *md = NULL;
+
+    if (!p_scossl_rsa_signverify_init(ctx, keyCtx, params, operation))
+    {
+        return SCOSSL_FAILURE;
+    }
+
+    // Sigalgs are not supported for PSS keys
+    if (ctx->padding == RSA_PKCS1_PSS_PADDING)
+    {
+        ERR_raise(ERR_LIB_PROV, PROV_R_OPERATION_NOT_SUPPORTED_FOR_THIS_KEYTYPE);
+        return SCOSSL_FAILURE;
+    }
+
+    const OSSL_ITEM *mdInfo = p_scossl_rsa_get_supported_md(ctx->libctx, RSA_PKCS1_PADDING, mdname, NULL, &md);
+    if (mdInfo == NULL)
+    {
+        ERR_raise(ERR_LIB_PROV, PROV_R_INVALID_DIGEST);
+        return SCOSSL_FAILURE;
+    }
+
+    EVP_MD_free(ctx->md);
+    ctx->md = md;
+    ctx->mdInfo = mdInfo;
+    ctx->padding = RSA_PKCS1_PADDING;
+    ctx->allowMdUpdates = FALSE;
+    ctx->isSigalg = TRUE;
+
+    return SCOSSL_SUCCESS;
+}
+
+static SCOSSL_STATUS p_scossl_rsa_sigalg_message_signverify_init(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx, _In_ SCOSSL_PROV_RSA_KEY_CTX *keyCtx,
+                                                                 _In_ const OSSL_PARAM params[], int operation,
+                                                                 _In_ const char *mdname)
+{
+    if (!p_scossl_rsa_sigalg_signverify_init(ctx, keyCtx, params, operation, mdname))
+    {
+        return SCOSSL_FAILURE;
+    }
+
+    if (ctx->mdctx == NULL)
+    {
+        if ((ctx->mdctx = EVP_MD_CTX_new()) == NULL)
+        {
+            ERR_raise(ERR_LIB_PROV, ERR_R_MALLOC_FAILURE);
+            return SCOSSL_FAILURE;
+        }
+    }
+
+    if (!EVP_DigestInit_ex2(ctx->mdctx, ctx->md, params))
+    {
+        EVP_MD_CTX_free(ctx->mdctx);
+        ctx->mdctx = NULL;
+        return SCOSSL_FAILURE;
+    }
+
+    return SCOSSL_SUCCESS;
+}
+
+static SCOSSL_STATUS p_scossl_rsa_sigalg_signverify_message_update(_In_ SCOSSL_RSA_SIGN_CTX *ctx,
+                                                                   _In_reads_bytes_(datalen) const unsigned char *data,
+                                                                   size_t datalen)
+{
+    if (ctx == NULL || ctx->mdctx == NULL)
+    {
+        ERR_raise(ERR_LIB_PROV, ERR_R_PASSED_NULL_PARAMETER);
+        return SCOSSL_FAILURE;
+    }
+
+    if (!ctx->allowUpdate)
+    {
+        ERR_raise(ERR_LIB_PROV, PROV_R_UPDATE_CALL_OUT_OF_ORDER);
+        return SCOSSL_FAILURE;
+    }
+
+    ctx->allowOneshot = 0;
+
+    return EVP_DigestUpdate(ctx->mdctx, data, datalen);
+}
+
+static SCOSSL_STATUS p_scossl_rsa_sigalg_sign_message_final(_In_ SCOSSL_RSA_SIGN_CTX *ctx,
+                                                            _Out_writes_bytes_opt_(*siglen) unsigned char *sig,
+                                                            _Out_ size_t *siglen, size_t sigsize)
+{
+    BYTE abDigest[EVP_MAX_MD_SIZE];
+    unsigned int cbDigest = 0;
+
+    if (ctx == NULL || ctx->mdctx == NULL)
+    {
+        ERR_raise(ERR_LIB_PROV, ERR_R_PASSED_NULL_PARAMETER);
+        return SCOSSL_FAILURE;
+    }
+
+    if (!ctx->allowFinal)
+    {
+        ERR_raise(ERR_LIB_PROV, PROV_R_FINAL_CALL_OUT_OF_ORDER);
+        return SCOSSL_FAILURE;
+    }
+
+    // If sig is NULL, this is a size fetch, and the digest does not need to be computed
+    if (sig != NULL)
+    {
+        if (!EVP_DigestFinal_ex(ctx->mdctx, abDigest, &cbDigest))
+        {
+            return SCOSSL_FAILURE;
+        }
+
+        ctx->allowUpdate = FALSE;
+        ctx->allowFinal = FALSE;
+        ctx->allowOneshot = FALSE;
+    }
+
+    return p_scossl_rsa_sign(ctx, sig, siglen, sigsize, abDigest, cbDigest);
+}
+
+static int p_scossl_rsa_sigalg_verify_message_final(_In_ SCOSSL_RSA_SIGN_CTX *ctx)
+{
+    BYTE abDigest[EVP_MAX_MD_SIZE];
+    unsigned int cbDigest = 0;
+
+    if (ctx == NULL || ctx->mdctx == NULL)
+    {
+        ERR_raise(ERR_LIB_PROV, ERR_R_PASSED_NULL_PARAMETER);
+        return SCOSSL_FAILURE;
+    }
+
+    if (!ctx->allowFinal)
+    {
+        ERR_raise(ERR_LIB_PROV, PROV_R_FINAL_CALL_OUT_OF_ORDER);
+        return SCOSSL_FAILURE;
+    }
+
+    if (!EVP_DigestFinal_ex(ctx->mdctx, abDigest, &cbDigest))
+    {
+        return SCOSSL_FAILURE;
+    }
+
+    ctx->allowUpdate = FALSE;
+    ctx->allowFinal = FALSE;
+    ctx->allowOneshot = FALSE;
+
+    return p_scossl_rsa_verify(ctx, ctx->pbSignature, ctx->cbSignature, abDigest, cbDigest);
+}
+
+static const char **p_scossl_rsa_sigalg_query_key_types(void)
+{
+    static const char *keyTypes[] = { "RSA", NULL };
+    return keyTypes;
+}
+
+#define IMPLEMENT_SCOSSL_RSA_SIGALG(md, MD)                                                                         \
+    static SCOSSL_STATUS p_scossl_rsa_##md##_sign_init(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx,                            \
+                                                       _In_ SCOSSL_PROV_RSA_KEY_CTX *keyCtx,                        \
+                                                       _In_ const OSSL_PARAM params[])                              \
+    {                                                                                                               \
+        return p_scossl_rsa_sigalg_signverify_init(ctx, keyCtx, params, EVP_PKEY_OP_SIGN, MD);                      \
+    }                                                                                                               \
+                                                                                                                    \
+    static SCOSSL_STATUS p_scossl_rsa_##md##_verify_init(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx,                          \
+                                                         _In_ SCOSSL_PROV_RSA_KEY_CTX *keyCtx,                      \
+                                                         _In_ const OSSL_PARAM params[])                            \
+    {                                                                                                               \
+        return p_scossl_rsa_sigalg_signverify_init(ctx, keyCtx, params, EVP_PKEY_OP_VERIFY, MD);                    \
+    }                                                                                                               \
+                                                                                                                    \
+    static SCOSSL_STATUS p_scossl_rsa_##md##_sign_message_init(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx,                    \
+                                                               _In_ SCOSSL_PROV_RSA_KEY_CTX *keyCtx,                \
+                                                               _In_ const OSSL_PARAM params[])                      \
+    {                                                                                                               \
+        return p_scossl_rsa_sigalg_message_signverify_init(ctx, keyCtx, params, EVP_PKEY_OP_SIGNMSG, MD);           \
+    }                                                                                                               \
+                                                                                                                    \
+    static SCOSSL_STATUS p_scossl_rsa_##md##_verify_message_init(_Inout_ SCOSSL_RSA_SIGN_CTX *ctx,                  \
+                                                                 _In_ SCOSSL_PROV_RSA_KEY_CTX *keyCtx,              \
+                                                                 _In_ const OSSL_PARAM params[])                    \
+    {                                                                                                               \
+        return p_scossl_rsa_sigalg_message_signverify_init(ctx, keyCtx, params, EVP_PKEY_OP_VERIFYMSG, MD);         \
+    }                                                                                                               \
+                                                                                                                    \
+    const OSSL_DISPATCH p_scossl_rsa_##md##_signature_functions[] = {                                               \
+        {OSSL_FUNC_SIGNATURE_NEWCTX,               (void (*)(void))p_scossl_rsa_newctx},                            \
+        {OSSL_FUNC_SIGNATURE_DUPCTX,               (void (*)(void))p_scossl_rsa_dupctx},                            \
+        {OSSL_FUNC_SIGNATURE_FREECTX,              (void (*)(void))p_scossl_rsa_freectx},                           \
+        {OSSL_FUNC_SIGNATURE_SIGN_INIT,            (void (*)(void))p_scossl_rsa_##md##_sign_init},                  \
+        {OSSL_FUNC_SIGNATURE_SIGN,                 (void (*)(void))p_scossl_rsa_sign},                              \
+        {OSSL_FUNC_SIGNATURE_SIGN_MESSAGE_INIT,    (void (*)(void))p_scossl_rsa_##md##_sign_message_init},          \
+        {OSSL_FUNC_SIGNATURE_SIGN_MESSAGE_UPDATE,  (void (*)(void))p_scossl_rsa_sigalg_signverify_message_update},  \
+        {OSSL_FUNC_SIGNATURE_SIGN_MESSAGE_FINAL,   (void (*)(void))p_scossl_rsa_sigalg_sign_message_final},         \
+        {OSSL_FUNC_SIGNATURE_VERIFY_INIT,          (void (*)(void))p_scossl_rsa_##md##_verify_init},                \
+        {OSSL_FUNC_SIGNATURE_VERIFY,               (void (*)(void))p_scossl_rsa_verify},                            \
+        {OSSL_FUNC_SIGNATURE_VERIFY_MESSAGE_INIT,  (void (*)(void))p_scossl_rsa_##md##_verify_message_init},        \
+        {OSSL_FUNC_SIGNATURE_VERIFY_MESSAGE_UPDATE,(void (*)(void))p_scossl_rsa_sigalg_signverify_message_update},  \
+        {OSSL_FUNC_SIGNATURE_VERIFY_MESSAGE_FINAL, (void (*)(void))p_scossl_rsa_sigalg_verify_message_final},       \
+        {OSSL_FUNC_SIGNATURE_QUERY_KEY_TYPES,      (void (*)(void))p_scossl_rsa_sigalg_query_key_types},            \
+        {OSSL_FUNC_SIGNATURE_GET_CTX_PARAMS,       (void (*)(void))p_scossl_rsa_get_ctx_params},                    \
+        {OSSL_FUNC_SIGNATURE_GETTABLE_CTX_PARAMS,  (void (*)(void))p_scossl_rsa_gettable_ctx_params},               \
+        {OSSL_FUNC_SIGNATURE_SET_CTX_PARAMS,       (void (*)(void))p_scossl_rsa_sigalg_set_ctx_params},             \
+        {OSSL_FUNC_SIGNATURE_SETTABLE_CTX_PARAMS,  (void (*)(void))p_scossl_rsa_sigalg_settable_ctx_params},        \
+        {0, NULL}};
+
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha1,       OSSL_DIGEST_NAME_SHA1)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha224,     OSSL_DIGEST_NAME_SHA2_224)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha256,     OSSL_DIGEST_NAME_SHA2_256)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha384,     OSSL_DIGEST_NAME_SHA2_384)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha512,     OSSL_DIGEST_NAME_SHA2_512)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha512_224, OSSL_DIGEST_NAME_SHA2_512_224)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha512_256, OSSL_DIGEST_NAME_SHA2_512_256)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha3_224,   OSSL_DIGEST_NAME_SHA3_224)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha3_256,   OSSL_DIGEST_NAME_SHA3_256)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha3_384,   OSSL_DIGEST_NAME_SHA3_384)
+IMPLEMENT_SCOSSL_RSA_SIGALG(sha3_512,   OSSL_DIGEST_NAME_SHA3_512)
 
 #ifdef __cplusplus
 }
